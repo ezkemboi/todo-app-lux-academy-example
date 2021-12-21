f981c160fcd27b820213eaa1a78d42abaa96af20
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2021-12-21 21:15:19 +0300

    demo useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import data from './data';
 
 function App() {
+  // useEffect -> when the an app first loads 
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState([]); 
+
+  const handleFetchData = async () => {
+    // set to state
+    setTodos(data);
+    setLoading(false);
+  }
+
+  // componentDidmount
+  useEffect(() => {
+    setTimeout(() => handleFetchData(), 1000) // call function that fetches data 
+  }, [])
+
+  // if one item changes, you make some changes -> when someone complete a todo, you make a request
+  // useEffect(() => {
+
+  // }, [status])
+
+  // console.log({ todos })
+
+  const handleCompleteTodo = (id) => {
+    console.log({ id })
+    let currentTodos = todos;
+    currentTodos.map(todo => {
+      if(todo.id === id) {
+        todo.status = 'complete'
+      }
+    })
+
+    setTodos(() => [...currentTodos]); // replace the state
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: '60px'
+      }}
+    >
+      {
+        loading ?
+        <p>Loading.......</p>
+        :
+        <div>
+          {
+            todos.map((todo) => {
+              return (
+                <div key={todo.id} style={{ padding: '20px' }}>
+                  <h4 style={{ fontColor: 'red'}}>{todo.name}</h4>
+                  <p>{todo.assignee}</p>
+                  <p>{todo.dueDate}</p>
+                  <p>{todo.status}</p>
+                  {
+                    todo.status === 'incomplete' &&
+                    <button style={{ cursor: "pointer" }} onClick={() => handleCompleteTodo(todo.id)}>
+                      Complete Todo
+                    </button>
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
+      }
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,25 @@
+const todos = [
+    {
+        id: 1,
+        name: 'Finish Homework',
+        assignee: "James",
+        dueDate: "22/12/2021",
+        status: "incomplete"
+    },
+    {
+        id: 2,
+        name: 'Riding Bicycle',
+        assignee: "Jane",
+        dueDate: "21/12/2021",
+        status: "complete"
+    },
+    {
+        id: 3,
+        name: 'Breakfast',
+        assignee: "Jane",
+        dueDate: "22/12/2021",
+        status: "incomplete"
+    },
+]
+
+export default todos;
